de54f4b78fc1e821703cbe9df75de217bce72720
rmsid1312 <rmsid1312@naver.com>
2025-09-06 22:51:39 +0900

    react query

--- src/starter/08-fetch-data/types.ts
+++ src/starter/08-fetch-data/types.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import axios from "axios";
 
+const url = "https://www.course-api.com/react-tours-project";
+
 export const tourSchema = z.object({
   id: z.string(),
   name: z.string(),
@@ -11,3 +13,13 @@ export const tourSchema = z.object({
 });
 
 export type Tour = z.infer<typeof tourSchema>;
+
+export const fetchTours = async (): Promise<Tour[]> => {
+  const resposne = await axios.get<Tour[]>(url);
+  const result = tourSchema.array().safeParse(resposne.data);
+  if (!result.success) {
+    console.log(result.error.message);
+    throw new Error(`Failed to parse tours`);
+  }
+  return result.data;
+};
